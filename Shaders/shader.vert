7e2fbfd41a3cac980a8ebaeb0f6b50cba201bca1
NutchapolSal <Nutchapol.sal@gmail.com>
2024-02-12 20:00:09 +0700

    init

---
+++ Shaders/shader.vert
@@ -0,0 +1,12 @@
+#version 330
+
+layout (location = 0) in vec3 pos;
+
+uniform mat4 model;
+out vec4 vCol;
+
+void main()
+{
+    gl_Position = model * vec4(pos, 1.0);
+    vCol = vec4(pos, 1.0);
+}
---
+++ main.cpp
@@ -0,0 +1,101 @@
+#undef GLFW_DLL
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <string>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <cmath>
+#include <vector>
+
+#include "Libs/Mesh.h"
+#include "Libs/Shader.h"
+#include "Libs/Window.h"
+
+const GLint WIDTH = 800, HEIGHT = 600;
+
+Window mainWindow;
+std::vector<Mesh *> meshList;
+std::vector<Shader> shaderList;
+
+// Vertex Shader
+static const char *vShader = "Shaders/shader.vert";
+
+// Fragment Shader
+static const char *fShader = "Shaders/shader.frag";
+
+void CreateTriangle() {
+    GLfloat vertices[] =
+        {
+            -1.0f, -1.0f, 0.0f,
+            0.0f, -1.0f, 1.0f,
+            1.0f, -1.0f, 0.0f,
+            0.0f, 1.0f, 0.0f};
+
+    unsigned int indices[] =
+        {
+            0, 3, 1,
+            1, 3, 2,
+            2, 3, 0,
+            0, 1, 2};
+
+    Mesh *obj1 = new Mesh();
+    obj1->CreateMesh(vertices, indices, 9, 9);
+    meshList.push_back(obj1);
+}
+
+void CreateShaders() {
+    Shader *shader1 = new Shader();
+    shader1->CreateFromFiles(vShader, fShader);
+    shaderList.push_back(*shader1);
+}
+
+int main() {
+    mainWindow = Window(WIDTH, HEIGHT, 3, 3);
+    mainWindow.initialise();
+
+    CreateTriangle();
+    CreateShaders();
+
+    GLuint uniformModel = 0;
+
+    // for secret room 3 enter - https://forms.gle/U9VE4pkYAPNvUW1H9
+
+    // Loop until window closed
+    while (!mainWindow.getShouldClose()) {
+        // Get + Handle user input events
+        glfwPollEvents();
+
+        // Clear window
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // draw here
+        shaderList[0].UseShader();
+        uniformModel = shaderList[0].GetUniformLocation("model");
+
+        glm::mat4 model(1.0f);
+        model = glm::scale(model, glm::vec3(0.4f, 0.4f, 1.0f));
+        model = glm::translate(model, glm::vec3(0.3f, 0.0f, -2.5f));
+
+        glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+
+        // Object
+        meshList[0]
+            ->RenderMesh();
+
+        glUseProgram(0);
+        // end draw
+
+        // magic word - SAKURA5
+
+        mainWindow.swapBuffers();
+    }
+
+    return 0;
+}
