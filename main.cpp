272662d06e30b46f25ebddcfac470a0f9d44a709
NutchapolSal <Nutchapol.sal@gmail.com>
2024-02-12 23:01:14 +0700

    complete week 8 instructions

--- main.cpp
+++ main.cpp
@@ -46,7 +46,9 @@ void CreateTriangle() {
 
     Mesh *obj1 = new Mesh();
     obj1->CreateMesh(vertices, indices, 12, 12);
-    meshList.push_back(obj1);
+    for (int i = 0; i < 10; i++) {
+        meshList.push_back(obj1);
+    }
 }
 
 void CreateShaders() {
@@ -64,7 +66,7 @@ int main() {
 
     // for secret room 3 enter - https://forms.gle/U9VE4pkYAPNvUW1H9
 
-    GLuint uniformModel = 0, uniformProjection = 0;
+    GLuint uniformModel = 0, uniformProjection = 0, uniformView = 0;
 
     glm::mat4 projection = glm::perspective(
         45.0f,
@@ -80,20 +82,50 @@ int main() {
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+        // camera
+        glm::mat4 view = glm::mat4(1.0f);
+        glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 10.0f);
+        glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, -1.0f);
+        glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+
+        glm::vec3 cameraDirection = glm::normalize(cameraTarget - cameraPos);
+
+        glm::vec3 cameraRight = glm::normalize(glm::cross(cameraDirection, up));
+        glm::vec3 cameraUp = glm::cross(cameraRight, cameraDirection);
+
         // draw here
         shaderList[0].UseShader();
         uniformModel = shaderList[0].GetUniformLocation("model");
+        uniformView = shaderList[0].GetUniformLocation("view");
         uniformProjection = shaderList[0].GetUniformLocation("projection");
 
-        // Object
-        glm::mat4 model(1.0f);
+        glm::vec3 pyramidPositions[] =
+            {
+                glm::vec3(0.0f, 0.0f, -2.5f),
+                glm::vec3(2.0f, 5.0f, -15.0f),
+                glm::vec3(-1.5f, -2.2f, -2.5f),
+                glm::vec3(-3.8f, -2.0f, -12.3f),
+                glm::vec3(2.4f, -0.4f, -3.5f),
+                glm::vec3(-1.7f, 3.0f, -7.5f),
+                glm::vec3(1.3f, -2.0f, -2.5f),
+                glm::vec3(1.5f, 2.0f, -2.5f),
+                glm::vec3(1.5f, 0.2f, -1.5f),
+                glm::vec3(-1.3f, 1.0f, -1.5f)};
 
-        model = glm::translate(model, glm::vec3(0.3f, 0.0f, -2.5f));
-        // model = glm::rotate(model, 90.0f * 3.1415f / 180.0f, glm::vec3(0.0f, 0.0f, 1.0f));
-        model = glm::scale(model, glm::vec3(0.4f, 0.4f, 1.0f));
-        glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
-        glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
-        meshList[0]->RenderMesh();
+        // Object
+        for (int i = 0; i < 10; i++) {
+            glm::mat4 model(1.0f);
+            glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraDirection, cameraUp);
+
+            model = glm::translate(model, pyramidPositions[i]);
+            model = glm::rotate(model, glm::radians(2.0f * i), glm::vec3(1.0f, 0.3f, 0.5f));
+            model = glm::scale(model, glm::vec3(0.8f, 0.8f, 1.0f));
+
+            glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+            glUniformMatrix4fv(uniformView, 1, GL_FALSE, glm::value_ptr(view));
+            glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
+            meshList[i]->RenderMesh();
+        }
 
         glUseProgram(0);
         // end draw
