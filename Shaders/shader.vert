52286a804ca700193938b02104b19fc1c934c588
NutchapolSal <Nutchapol.sal@gmail.com>
2024-02-12 21:42:57 +0700

    complete week 7 instructions 1

--- Shaders/shader.vert
+++ Shaders/shader.vert
@@ -2,7 +2,11 @@
 
 layout (location = 0) in vec3 pos;
 
+uniform mat4 model;
+uniform mat4 projection;
+
+
 void main()
 {
-    gl_Position = vec4(0.4 * pos.x, 0.4 * pos.y, pos.z, 1.0);
+    gl_Position = projection * model * vec4(pos, 1.0);
 }
--- main.cpp
+++ main.cpp
@@ -1,87 +1,103 @@
 #undef GLFW_DLL
 #include <iostream>
 #include <stdio.h>
-#include <string>
 #include <string.h>
+#include <string>
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
-#include <vector>
 #include <cmath>
+#include <vector>
 
+#include "Libs/Mesh.h"
 #include "Libs/Shader.h"
 #include "Libs/Window.h"
-#include "Libs/Mesh.h"
 
 const GLint WIDTH = 800, HEIGHT = 600;
 
 Window mainWindow;
-std::vector<Mesh*> meshList;
+std::vector<Mesh *> meshList;
 std::vector<Shader> shaderList;
 
-//Vertex Shader
-static const char* vShader = "Shaders/shader.vert";
+// Vertex Shader
+static const char *vShader = "Shaders/shader.vert";
 
-//Fragment Shader
-static const char* fShader = "Shaders/shader.frag";
+// Fragment Shader
+static const char *fShader = "Shaders/shader.frag";
 
-void CreateTriangle()
-{
+void CreateTriangle() {
     GLfloat vertices[] =
-    {
-        -1.0f, -1.0f, 0.0f,
-        1.0f, -1.0f, 0.0f,
-        0.0f, 1.0f, 0.0f
-    };
+        {
+            -1.0f, -1.0f, 0.0f,
+            1.0f, -1.0f, 0.0f,
+            0.0f, 1.0f, 0.0f};
 
-    unsigned int indices[] = 
-    {
-        0, 1, 2,
-    };
+    unsigned int indices[] =
+        {
+            0,
+            1,
+            2,
+        };
 
     Mesh *obj1 = new Mesh();
     obj1->CreateMesh(vertices, indices, 9, 9);
     meshList.push_back(obj1);
 }
 
-void CreateShaders()
-{
-    Shader* shader1 = new Shader();
+void CreateShaders() {
+    Shader *shader1 = new Shader();
     shader1->CreateFromFiles(vShader, fShader);
     shaderList.push_back(*shader1);
 }
 
-int main()
-{
+int main() {
     mainWindow = Window(WIDTH, HEIGHT, 3, 3);
     mainWindow.initialise();
 
     CreateTriangle();
     CreateShaders();
 
-    //for secret room 3 enter - https://forms.gle/U9VE4pkYAPNvUW1H9 
+    // for secret room 3 enter - https://forms.gle/U9VE4pkYAPNvUW1H9
 
-    //Loop until window closed
-    while (!mainWindow.getShouldClose())
-    {
-        //Get + Handle user input events
+    GLuint uniformModel = 0, uniformProjection = 0;
+
+    glm::mat4 projection = glm::perspective(
+        45.0f,
+        (GLfloat)mainWindow.getBufferWidth() / (GLfloat)mainWindow.getBufferHeight(),
+        0.1f,
+        100.0f);
+    // Loop until window closed
+    while (!mainWindow.getShouldClose()) {
+        // Get + Handle user input events
         glfwPollEvents();
 
-        //Clear window
+        // Clear window
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        //draw here
+        // draw here
         shaderList[0].UseShader();
+        uniformModel = shaderList[0].GetUniformLocation("model");
+        uniformProjection = shaderList[0].GetUniformLocation("projection");
 
-        //Object
+        // Object
+        glm::mat4 model(1.0f);
+
+        model = glm::translate(model, glm::vec3(0.3f, 0.0f, -2.5f));
+        model = glm::rotate(model, 90.0f * 3.1415f / 180.0f, glm::vec3(0.0f, 0.0f, 1.0f));
+        model = glm::scale(model, glm::vec3(0.4f, 0.4f, 1.0f));
+        glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+        glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
         meshList[0]->RenderMesh();
 
         glUseProgram(0);
-        //end draw
+        // end draw
 
-        //magic word - SAKURA
+        // magic word - SAKURA
 
         mainWindow.swapBuffers();
     }
