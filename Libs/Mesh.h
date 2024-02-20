f617861d1286472493a0b88586b1a5fa6253beed
NutchapolSal <Nutchapol.sal@gmail.com>
2024-02-20 10:36:34 +0700

    start week 10

--- Libs/Mesh.h
+++ Libs/Mesh.h
@@ -2,6 +2,22 @@
 #define MESH____H
 
 #include <GL/glew.h>
+#include <vector>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+#include <string>
+#include <string.h>
+
+struct Face {
+    int vIndex[3], vtIndex[3], vnIndex[3];
+};
 
 class Mesh
 {
@@ -12,9 +28,10 @@ class Mesh
         void CreateMesh(GLfloat* vertices, unsigned int* indices, unsigned int numOfVertices, unsigned int numOfIndices);
         void RenderMesh();
         void ClearMesh();
+        bool CreateMeshFromOBJ(const char * path);
 
     private:
-        GLuint VAO, VBO, IBO;
+        GLuint VAO, VBO, IBO, vertexBuffer, uvBuffer, normalBuffer;
         GLsizei indexCount;
 };
 
